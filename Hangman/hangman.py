11e7e469e768357b88fa1e73d57c3d1cfde55383
reetesh818 <reet.ug19cse18@opju.ac.in>
2022-02-12 12:41:20 +0530

    added lives to limit number of guesses

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -21,20 +21,28 @@ def hangman():
     alphabet = set(string.ascii_uppercase) #this will contain the set of uppercase alphabets
     used_letters = set() #this will contain the set of words the user has guessed
 
-    while len(word_letters) > 0:
+    lives = 6 #to limit the number of guesses by a particular user
+
+    while len(word_letters) > 0 and lives > 0:
         #we will keep on repeating these tasks unless our set of word letters is empty
         #once the set of word letters is empty it tells that all letters have been guessed correctly
 
         #this statemnt to show the letters which we have used already
-        print("You have used these letters:",' '.join(used_letters))
+        print("You have",lives,"lives and you have used these letters:",' '.join(used_letters))
 
-       
+          #what current word is
+        word_list =[letter if letter in used_letters else '_' for letter in word]
+        print('Current word:',' '.join(word_list))
+
 
         user_letter = input('Guess a letter:').upper() #this will take a letter as input from the user
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                lives -= 1
+                print("Letter is not in word.")
 
         elif user_letter in used_letters:
             print("You have already used that character.Please try again.")
@@ -42,9 +50,11 @@ def hangman():
         else:
             print("You typed an invalid character!")
 
-         #what current word is
-        word_list =[letter if letter in used_letters else '_' for letter in word]
-        print('Current word:',' '.join(word_list))
+
+    if lives == 0:
+            print('OOPS!Sorry, You are Dead! The correct word was:',word)
+    else:
+            print("Congratulations! You guessd the right word:",word)
 
 
 
